bafabb5757ebbb720f12ff23dfeea4eb4ed9ec07
Edom Rubinshtein <e18122@ganyavne.ort.org.il>
2025-05-12 11:24:46 +0300

    url update

--- backend/App.py
+++ backend/App.py
@@ -10,7 +10,7 @@ from werkzeug.utils import secure_filename
 
 # Initialize app and config
 app = Flask(__name__)
-CORS(app, origins=["https://tripping-je632nepn-novs-projects-c22b7ac3.vercel.app"], supports_credentials=True)
+CORS(app, origins=["http://localhost:5173"], supports_credentials=True)
 DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL:
     app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
